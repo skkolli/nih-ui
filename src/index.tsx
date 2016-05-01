6a64e5459eedc5dec676ba6b26b8cf9d8043a90d
Satish Kolli <satish.kolli@gmail.com>
2016-05-01 08:41:47 -0400

    deleted all references to demo urls.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import {App} from "./app/src/App.tsx";
 import {TaxonomyApp} from "./app/src/TaxonomyApp.tsx";
 import {CrimeApp} from "./app/src/crime/CrimeApp.tsx";
-import {Demo1} from "./demo/demo1.tsx"
-import {Demo2} from "./demo/demo2.tsx";
 import {ListApp} from "./app/src/list-app/ListApp.tsx";
 
 import {Router, Route, IndexRoute} from "react-router";
@@ -17,8 +15,6 @@ ReactDOM.render((
     <Route component={TaxonomyApp} path="taxonomy"/>
     <Route component={CrimeApp} path="crime"/>
 
-    <Route component={Demo1} path="demo/demo1"/>
-    <Route component={Demo2} path="demo/demo2"/>
 
     <Route component={ListApp} path="list-app"/>
   </Router>
